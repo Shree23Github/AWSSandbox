9da69ba71da044a1230349616fc42c7aaa5519c3
Shourya Shree <shreeshourya23@gmail.com>
2025-03-10 13:58:39 +0530

    task04

--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -2,6 +2,7 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
@@ -15,13 +16,17 @@ import java.util.Map;
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
+@SqsTriggerEventSource(
+		targetQueue = "async_queue",
+		batchSize = 10
+)
 public class SqsHandler implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
-		context.getLogger().log("Received event: " + request.toString() + "\n");
+		System.out.println("Hello from lambda");
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Processed SQS Messages Successfully");
+		resultMap.put("body", "Hello from Lambda");
 		return resultMap;
 	}
 }
